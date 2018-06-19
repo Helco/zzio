e94f868636c0fe1a77e851cc33611b094fa05ca7
Helco <hermann.noll@hotmail.com>
2018-06-19 21:04:32 +0200

    Add intToEnum/Flags

---
+++ zzio.tests/zzio/utils/TestEnumUtils.cs
@@ -0,0 +1,45 @@
+using System;
+using NUnit.Framework;
+using zzio.utils;
+
+namespace zzio.tests.utils
+{
+    [TestFixture]
+    public class TestEnumUtils
+    {
+        private enum TestEnum
+        {
+            A = 0,
+            B = 100,
+            C = 300,
+            Unknown = -1
+        }
+
+        [Flags]
+        private enum TestFlags
+        {
+            A = 1 << 0,
+            B = 1 << 1,
+            C = 1 << 5
+        }
+
+        [Test]
+        public void intToEnum()
+        {
+            Assert.AreEqual(TestEnum.A, EnumUtils.intToEnum<TestEnum>(0));
+            Assert.AreEqual(TestEnum.B, EnumUtils.intToEnum<TestEnum>(100));
+            Assert.AreEqual(TestEnum.Unknown, EnumUtils.intToEnum<TestEnum>(200));
+            Assert.AreEqual(TestEnum.Unknown, EnumUtils.intToEnum<TestEnum>(-1));
+        }
+
+        [Test]
+        public void intToFlags()
+        {
+            Assert.AreEqual("0", EnumUtils.intToFlags<TestFlags>(0).ToString());
+            Assert.AreEqual("A", EnumUtils.intToFlags<TestFlags>(1).ToString());
+            Assert.AreEqual("A, B, C", EnumUtils.intToFlags<TestFlags>(1 + 2 + 32).ToString());
+            Assert.AreEqual("A, B", EnumUtils.intToFlags<TestFlags>(1 + 2 + 4).ToString());
+            Assert.AreEqual("0", EnumUtils.intToFlags<TestFlags>(4 + 16).ToString());
+        }
+    }
+}
---
+++ zzio/utils/EnumUtils.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+
+namespace zzio.utils {
+    public static class EnumUtils
+    {
+        public static T intToEnum<T>(int i) where T : struct, IConvertible
+        {
+            if (Enum.IsDefined(typeof(T), i))
+                return (T)Enum.Parse(typeof(T), i.ToString());
+            else
+                return (T)Enum.Parse(typeof(T), "Unknown");
+        }
+
+        public static T intToFlags<T>(uint value) where T : struct, IConvertible
+        {
+            string flagString = "";
+            for (int bit = 0; bit < 32; bit++)
+            {
+                int intFlag = 1 << bit;
+                if ((value & intFlag) > 0 && Enum.IsDefined(typeof(T), intFlag))
+                    flagString += "," + Enum.Parse<T>(intFlag.ToString()).ToString();
+            }
+            if (flagString.Length == 0)
+                return default(T);
+            else
+                return Enum.Parse<T>(flagString.Substring(1));
+        }
+    }
+}
