835327e6e3bc6d297843e7e2f006252793436d5f
Helco <hermann.noll@hotmail.com>
2018-07-01 12:35:47 +0200

    Add VirtualFileSystem

---
+++ zzio.tests/zzio/vfs/TestVirtualFileSystem.cs
@@ -0,0 +1,125 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+using zzio.vfs;
+
+namespace zzio.tests.vfs
+{
+    [TestFixture]
+    public class TestVirtualFileSystem
+    {
+        private readonly VirtualFileSystem vfs;
+        private readonly DummyResourcePool pool1;
+        private readonly DummyResourcePool pool2;
+
+        public TestVirtualFileSystem()
+        {
+            pool1 = new DummyResourcePool(new string[]
+            {
+                "content.txt",
+                "a/d.txt",
+                "c/a.txt",
+                "d/e/f.txt",
+                "d/e/g.txt",
+                "d/h/i.txt"
+            }, new byte[] { 1, 2, 3, 4 });
+
+            pool2 = new DummyResourcePool(new string[]
+            {
+                "content.txt",
+                "a/b.txt",
+                "a/c.txt",
+                "b/a.txt"
+            }, new byte[] { 5, 6, 7, 8 });
+
+            vfs = new VirtualFileSystem();
+            vfs.AddResourcePool(pool1);
+            vfs.AddResourcePool(pool2);
+        }
+
+        [Test]
+        public void getresourcetype()
+        {
+            Assert.AreEqual(ResourceType.File, vfs.GetResourceType("content.txt"));
+            Assert.AreEqual(ResourceType.File, vfs.GetResourceType("a/d.txt"));
+            Assert.AreEqual(ResourceType.File, vfs.GetResourceType("c/a.txt"));
+            Assert.AreEqual(ResourceType.File, vfs.GetResourceType("d/E\\f.txt"));
+            Assert.AreEqual(ResourceType.File, vfs.GetResourceType("d/e/sdf\\\\.././g.txt"));
+            Assert.AreEqual(ResourceType.File, vfs.GetResourceType("d/h/i.txt"));
+            Assert.AreEqual(ResourceType.File, vfs.GetResourceType("a/b.txt"));
+            Assert.AreEqual(ResourceType.File, vfs.GetResourceType("a/c.txt"));
+            Assert.AreEqual(ResourceType.File, vfs.GetResourceType("b/a.txt"));
+
+            Assert.AreEqual(ResourceType.Directory, vfs.GetResourceType("a/"));
+            Assert.AreEqual(ResourceType.Directory, vfs.GetResourceType("d/e"));
+            Assert.AreEqual(ResourceType.Directory, vfs.GetResourceType("b"));
+
+            Assert.AreEqual(ResourceType.NonExistant, vfs.GetResourceType("asd"));
+            Assert.AreEqual(ResourceType.NonExistant, vfs.GetResourceType("d/e/asldkj"));
+        }
+
+        private void testStream(byte[] expected, Stream stream)
+        {
+            byte[] actual = new byte[expected.Length];
+            Assert.NotNull(stream);
+            Assert.AreEqual(4, stream.Read(actual, 0, actual.Length));
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(-1, stream.ReadByte());
+        }
+
+        [Test]
+        public void getfilecontent()
+        {
+            testStream(new byte[] { 1, 2, 3, 4 }, vfs.GetFileContent("a/d.txt"));
+            testStream(new byte[] { 1, 2, 3, 4 }, vfs.GetFileContent("c/a.txt"));
+            testStream(new byte[] { 1, 2, 3, 4 }, vfs.GetFileContent("d/e/f.txt"));
+            testStream(new byte[] { 1, 2, 3, 4 }, vfs.GetFileContent("d/e/g.txt"));
+            testStream(new byte[] { 1, 2, 3, 4 }, vfs.GetFileContent("d/h/i.txt"));
+
+            testStream(new byte[] { 5, 6, 7, 8 }, vfs.GetFileContent("a/b.txt"));
+            testStream(new byte[] { 5, 6, 7, 8 }, vfs.GetFileContent("a/c.txt"));
+            testStream(new byte[] { 5, 6, 7, 8 }, vfs.GetFileContent("b/a.txt"));
+
+            testStream(new byte[] { 1, 2, 3, 4 }, vfs.GetFileContent("content.txt"));
+            testStream(new byte[] { 1, 2, 3, 4 }, vfs.GetFileContent(".//\\\\b/../a/./d.txt"));
+
+            Assert.IsNull(vfs.GetFileContent("nope"));
+            Assert.IsNull(vfs.GetFileContent("a/a.txt"));
+            Assert.IsNull(vfs.GetFileContent("a/../b/../d/e/j.txt"));
+        }
+
+        [Test]
+        public void getdirectorycontent()
+        {
+            string[] rootDirContent = new string[]
+            {
+                "content.txt",
+                "a",
+                "c",
+                "d",
+                "b"
+            };
+            Assert.AreEqual(rootDirContent, vfs.GetDirectoryContent(""));
+            Assert.AreEqual(rootDirContent, vfs.GetDirectoryContent("."));
+            Assert.AreEqual(rootDirContent, vfs.GetDirectoryContent("./"));
+            Assert.AreEqual(rootDirContent, vfs.GetDirectoryContent(".\\"));
+            Assert.AreEqual(rootDirContent, vfs.GetDirectoryContent("./b/../asdasd\\..\\\\///"));
+
+            Assert.AreEqual(new string[]
+            {
+                "d.txt",
+                "b.txt",
+                "c.txt"
+            }, vfs.GetDirectoryContent("a"));
+            
+            Assert.AreEqual(new string[] { "a.txt" }, vfs.GetDirectoryContent("c"));
+            Assert.AreEqual(new string[] { "e", "h" }, vfs.GetDirectoryContent("d"));
+            Assert.AreEqual(new string[] { "a.txt" }, vfs.GetDirectoryContent("b"));
+
+            Assert.AreEqual(new string[0], vfs.GetDirectoryContent("content.txt"));
+            Assert.AreEqual(new string[0], vfs.GetDirectoryContent("nopenopenope"));
+            Assert.AreEqual(new string[0], vfs.GetDirectoryContent(".."));
+            Assert.AreEqual(new string[0], vfs.GetDirectoryContent("a/b/../c//d"));
+        }
+    }
+}
---
+++ zzio/vfs/VirtualFileSystem.cs
@@ -0,0 +1,110 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using zzio.utils;
+
+namespace zzio.vfs
+{
+    public class VirtualFileSystem
+    {
+        private static readonly Dictionary<string, Func<string, IResourcePool>> resourcePoolTypes =
+            new Dictionary<string, Func<string, IResourcePool>>()
+            {
+                { "file", (path) => new FileResourcePool(path) }
+            };
+
+        private List<IResourcePool> pools = new List<IResourcePool>();
+
+        public void AddResourcePool(string type, string path)
+        {
+            if (!resourcePoolTypes.ContainsKey(type.ToLower()))
+                throw new NotSupportedException("Resource pool type \"" + type + "\" is not supported");
+            pools.Add(resourcePoolTypes[type](path));
+        }
+
+        public void AddResourcePool(IResourcePool pool)
+        {
+            pools.Add(pool);
+        }
+
+        /// finds a case-insensitive resource in a (possibly) case-sensitive pool
+        /// <returns>UB if `resourceType == NonExistant`</returns>
+        private static FilePath findResourceIn(string pathString, IResourcePool pool, out ResourceType resourceType)
+        {
+            FilePath path = new FilePath(pathString).Normalized;
+            // naive try
+            resourceType = pool.GetResourceType(path.ToPOSIXString());
+            if (resourceType != ResourceType.NonExistant)
+                return path;
+
+            // combine parts one by one
+            StringComparison ignoreCase = StringComparison.InvariantCultureIgnoreCase;
+            string[] pathParts = path.Parts;
+            FilePath curPath = new FilePath("");
+            try
+            {
+                foreach (string searchPart in pathParts)
+                {
+                    string goodPart = pool
+                        .GetDirectoryContent(curPath.ToPOSIXString())
+                        .First((part) => part.Equals(searchPart, ignoreCase));
+                    curPath = curPath.Combine(goodPart);
+                }
+                resourceType = pool.GetResourceType(curPath.ToPOSIXString());
+            }
+            catch (InvalidOperationException)
+            {
+                return null; // when no matching file/dir could be found
+            }
+            catch (IOException)
+            {
+                return null;
+            }
+            return curPath;
+        }
+
+        public ResourceType GetResourceType(string path)
+        {
+            foreach (IResourcePool pool in pools)
+            {
+                ResourceType type;
+                findResourceIn(path, pool, out type);
+                if (type != ResourceType.NonExistant)
+                    return type;
+            }
+            return ResourceType.NonExistant;
+        }
+
+        public Stream GetFileContent(string path)
+        {
+            foreach (IResourcePool pool in pools)
+            {
+                ResourceType type;
+                FilePath casePath = findResourceIn(path, pool, out type);
+                if (type != ResourceType.File)
+                    continue;
+                Stream stream = pool.GetFileContent(casePath.ToPOSIXString());
+                if (stream != null)
+                    return stream;
+            }
+            return null;
+        }
+
+        public string[] GetDirectoryContent(string path)
+        {
+            IEnumerable<string> content = new HashSet<string>();
+            foreach (IResourcePool pool in pools)
+            {
+                ResourceType type;
+                FilePath casePath = findResourceIn(path, pool, out type);
+                if (type != ResourceType.Directory)
+                    continue;
+                content = content.Union(
+                    pool.GetDirectoryContent(casePath.ToPOSIXString())
+                );
+            }
+            return content.ToArray();
+        }
+    }
+}
