f07ad99dc323468f6dbfb2620fddabd07a9562f6
Helco <hermann.noll@hotmail.com>
2018-07-04 20:03:33 +0200

    DB Refactor: Add Row

---
+++ zzio/db/Row.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using zzio.primitives;
+using zzio.utils;
+
+namespace zzio.db
+{
+    [Serializable]
+    public class Row
+    {
+        public UID uid;
+        public Cell[] cells = new Cell[0];
+
+        public Row() { }
+
+        public void Read(BinaryReader reader)
+        {
+            uid = UID.ReadNew(reader);
+            cells = new Cell[reader.ReadUInt32()];
+            for (int i = 0; i < cells.Length; i++)
+                cells[i] = Cell.ReadNew(reader);
+        }
+
+        public void Write(BinaryWriter writer)
+        {
+            uid.Write(writer);
+            writer.Write(cells.Length);
+            foreach (Cell cell in cells)
+                cell.Write(writer);
+        }
+    }
+}
