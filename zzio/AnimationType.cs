b6641c1f361ea4f97b28d51936db4b6b53273797
Helco <hermann.noll@hotmail.com>
2018-10-28 14:34:36 +0100

    Add AnimationType.IsLooping

--- zzio/AnimationType.cs
+++ zzio/AnimationType.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace zzio
 {
     public enum AnimationType
@@ -43,5 +45,58 @@ namespace zzio
         ThudGround2,
         PixieFlounder,
         JumpHigh
+    }
+
+    public static class AnimationTypeExtension
+    {
+        private static readonly IReadOnlyDictionary<AnimationType, bool> isLooping =
+            new Dictionary<AnimationType, bool>()
+            {
+                { AnimationType.Idle0,              true },
+                { AnimationType.Jump,               false },
+                { AnimationType.Run,                true },
+                { AnimationType.RunForwardLeft,     true },
+                { AnimationType.RunForwardRight,    true },
+                { AnimationType.Back,               true },
+                { AnimationType.Dance,              true },
+                { AnimationType.Fall,               true },
+                { AnimationType.Rotate,             true },
+                { AnimationType.Right,              true },
+                { AnimationType.Left,               true },
+                { AnimationType.Idle1,              true },
+                { AnimationType.Idle2,              true },
+                { AnimationType.Talk0,              false },
+                { AnimationType.Talk1,              false },
+                { AnimationType.Talk2,              false },
+                { AnimationType.Talk3,              false },
+                { AnimationType.Walk0,              true },
+                { AnimationType.Walk1,              true },
+                { AnimationType.Walk2,              true },
+                { AnimationType.SpecialIdle0,       true },
+                { AnimationType.SpecialIdle1,       true },
+                { AnimationType.SpecialIdle2,       true },
+                { AnimationType.FlyForward,         true },
+                { AnimationType.FlyBack,            true },
+                { AnimationType.FlyLeft,            true },
+                { AnimationType.FlyRight,           true },
+                { AnimationType.Loadup,             true },
+                { AnimationType.Hit,                false },
+                { AnimationType.Joy,                true },
+                { AnimationType.ThudGround,         false },
+                { AnimationType.UseFairyPipe,       false },
+                { AnimationType.UseSeaShel,         false },
+                { AnimationType.Smith,              true },
+                { AnimationType.Astonished,         false },
+                { AnimationType.Surprise0,          true },
+                { AnimationType.Surprise1,          true },
+                { AnimationType.Stop,               false },
+                { AnimationType.ThudGround2,        false },
+                { AnimationType.PixieFlounder,      true },
+                { AnimationType.JumpHigh,           false }
+            };
+        public static bool IsLooping(this AnimationType type)
+        {
+            return isLooping[type];
+        }
     }
 }
