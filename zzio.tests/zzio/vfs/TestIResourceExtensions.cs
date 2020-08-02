cd3258f64947d28c65d8a6179af54fb59b6bb3b0
Helco <hermann.noll@hotmail.com>
2020-08-02 21:48:21 +0200

    Add FindAndOpen for IResource

---
+++ zzio.tests/zzio/vfs/TestIResourceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using zzio.tests.vfs;
+using zzio.vfs;
+
+namespace zzio.tests.zzio.vfs
+{
+    [TestFixture]
+    public class TestIResourceExtensions
+    {
+        [Test]
+        public void findAndOpen()
+        {
+            var pool = PoolResources.CombinedResourcePool;
+
+            MyAssert.Equals("common from b", pool.FindAndOpen("COMMON/CONTENT.txt"));
+            MyAssert.Equals("from b", pool.FindAndOpen("content.txt"));
+            Assert.IsNull(pool.FindAndOpen("common/c.txt"));
+            Assert.IsNull(pool.FindAndOpen("hello.txt"));
+        }
+    }
+}
---
+++ zzio/vfs/IResourceExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using zzio.utils;
+
+namespace zzio.vfs
+{
+    public static class IResourceExtensions
+    {
+        public static Stream? FindAndOpen(this IResourcePool pool, string pathText) => FindAndOpen(pool.Root, new FilePath(pathText));
+        public static Stream? FindAndOpen(this IResourcePool pool, FilePath path) => FindAndOpen(pool.Root, path);
+        public static Stream? FindAndOpen(this IResource res, string pathText) => FindAndOpen(res, new FilePath(pathText));
+        public static Stream? FindAndOpen(this IResource res, FilePath path)
+        {
+            if (path.IsAbsolute || !path.StaysInbound)
+                throw new ArgumentException("Invalid file path", nameof(path));
+            if (res.Type != ResourceType.Directory)
+                throw new ArgumentException("Root resource has to be a directory", nameof(res));
+
+            var comparison = StringComparison.InvariantCultureIgnoreCase;
+            var curDir = res;
+            foreach (var nextDirName in path.Parts.SkipLast(1))
+            {
+                var nextDir = curDir.Directories.FirstOrDefault(r => r.Name.Equals(nextDirName, comparison));
+                if (nextDir == null)
+                    return null;
+                curDir = nextDir;
+            }
+
+            var fileName = path.Parts.Last();
+            var file = curDir.Files.FirstOrDefault(r => r.Name.Equals(fileName, comparison));
+            return file?.OpenContent();
+        }
+    }
+}
