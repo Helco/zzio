b222762683fc688eff55ecb264c961fd3e266ed8
Helco <hermann.noll@hotmail.com>
2020-08-21 18:55:05 +0200

    More knowledge about RWPlaneSection

--- zzio/rwbs/RWPlaneSection.cs
+++ zzio/rwbs/RWPlaneSection.cs
@@ -7,20 +7,29 @@ using zzio.primitives;
 
 namespace zzio.rwbs
 {
-    [Serializable]
+    public enum RWPlaneSectionType
+    {
+        // *Plane has a *normal* vector in * direction
+
+        XPlane = 0,
+        ZPlane = 8,
+
+        Unknown = -1
+    }
+
     public class RWPlaneSection : StructSection
     {
         public override SectionId sectionId => SectionId.PlaneSection;
 
-        public UInt32 sectorType; // unknown enum
-        public float value, leftValue, rightValue;
+        public RWPlaneSectionType sectorType; // unknown enum
+        public float centerValue, leftValue, rightValue;
         public bool leftIsWorldSector, rightIsWorldSector;
 
         protected override void readStruct(Stream stream)
         {
             BinaryReader reader = new BinaryReader(stream);
-            sectorType = reader.ReadUInt32();
-            value = reader.ReadSingle();
+            sectorType = EnumUtils.intToEnum<RWPlaneSectionType>(reader.ReadInt32());
+            centerValue = reader.ReadSingle();
             leftIsWorldSector = reader.ReadUInt32() > 0;
             rightIsWorldSector = reader.ReadUInt32() > 0;
             leftValue = reader.ReadSingle();
@@ -30,8 +39,8 @@ namespace zzio.rwbs
         protected override void writeStruct(Stream stream)
         {
             BinaryWriter writer = new BinaryWriter(stream);
-            writer.Write(sectorType);
-            writer.Write(value);
+            writer.Write((int)sectorType);
+            writer.Write(centerValue);
             writer.Write((UInt32)(leftIsWorldSector ? 1 : 0));
             writer.Write((UInt32)(rightIsWorldSector ? 1 : 0));
             writer.Write(leftValue);
