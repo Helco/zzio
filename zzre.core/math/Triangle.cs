889c86f4b1f5bd631ff2e6f44fa99f9eb7461396
Helco <hermann.noll@hotmail.com>
2021-10-04 19:33:48 +0200

    Fix Barycentric for degenerate triangles

--- zzre.core/math/Triangle.cs
+++ zzre.core/math/Triangle.cs
@@ -111,6 +111,8 @@ namespace zzre
         {
             if (Sse41.IsSupported)
                 return BarycentricSse41(point);
+            if (CmpZero(Vector3.Cross(AB.Vector, AC.Vector).LengthSquared()))
+                return new Vector3(-1f);
             var AP_Vector = point - A;
             float d00 = Vector3.Dot(AB.Vector, AB.Vector);
             float d01 = Vector3.Dot(AB.Vector, AC.Vector);
@@ -119,7 +121,7 @@ namespace zzre
             float d21 = Vector3.Dot(AP_Vector, AC.Vector);
             float denom = d00 * d11 - d01 * d01;
             if (CmpZero(denom))
-                return Vector3.Zero;
+                return new Vector3(-1f); // more obvious than Zero
 
             Vector3 result;
             result.Y = (d11 * d20 - d01 * d21) / denom;
@@ -128,32 +130,45 @@ namespace zzre
             return result;
         }
 
-        private unsafe Vector3 BarycentricSse41(Vector3 point)
+        private Vector3 BarycentricSse41(Vector3 point)
         {
-            fixed (Triangle* thiz = &this)
-            {
-                var a = Sse.LoadVector128((float*)&thiz->A);
-                var b = Sse.LoadVector128((float*)&thiz->B);
-                var c = Sse.LoadVector128((float*)&thiz->C);
-                var p = Sse.LoadVector128((float*)&point);
-                var ab = Sse.Subtract(b, a);
-                var ac = Sse.Subtract(c, a);
-                var ap = Sse.Subtract(p, a);
-                float d00 = Sse41.DotProduct(ab, ab, 0b01110001).GetElement(0);
-                float d01 = Sse41.DotProduct(ab, ac, 0b01110001).GetElement(0);
-                float d11 = Sse41.DotProduct(ac, ac, 0b01110001).GetElement(0);
-                float d20 = Sse41.DotProduct(ap, ab, 0b01110001).GetElement(0);
-                float d21 = Sse41.DotProduct(ap, ac, 0b01110001).GetElement(0);
-                float denom = d00 * d11 - d01 * d01;
-                if (CmpZero(denom))
-                    return Vector3.Zero;
+            var a = A.AsVector128();
+            var b = B.AsVector128();
+            var c = C.AsVector128();
+            var p = point.AsVector128();
+            var ab = Sse.Subtract(b, a);
+            var ac = Sse.Subtract(c, a);
+            var abxac = CrossProductSse41(ab, ac);
+            var abxac_len = Sse41.DotProduct(abxac, abxac, 0b01110001).GetElement(0);
+            if (CmpZero(abxac_len))
+                return new Vector3(-1f);
 
-                Vector3 result;
-                result.Y = (d11 * d20 - d01 * d21) / denom;
-                result.Z = (d00 * d21 - d01 * d20) / denom;
-                result.X = 1f - result.Y - result.Z;
-                return result;
-            }
+            var ap = Sse.Subtract(p, a);
+            float d00 = Sse41.DotProduct(ab, ab, 0b01110001).GetElement(0);
+            float d01 = Sse41.DotProduct(ab, ac, 0b01110001).GetElement(0);
+            float d11 = Sse41.DotProduct(ac, ac, 0b01110001).GetElement(0);
+            float d20 = Sse41.DotProduct(ap, ab, 0b01110001).GetElement(0);
+            float d21 = Sse41.DotProduct(ap, ac, 0b01110001).GetElement(0);
+            float denom = d00 * d11 - d01 * d01;
+            if (CmpZero(denom))
+                return new Vector3(-1f);
+
+            Vector3 result;
+            result.Y = (d11 * d20 - d01 * d21) / denom;
+            result.Z = (d00 * d21 - d01 * d20) / denom;
+            result.X = 1f - result.Y - result.Z;
+            return result;
+        }
+
+        private Vector128<float> CrossProductSse41(Vector128<float> a, Vector128<float> b)
+        {
+            // based on https://geometrian.com/programming/tutorials/cross-product/index.php method 5
+            var tmp0 = Sse.Shuffle(a, a, 0b11001001);
+            var tmp1 = Sse.Shuffle(b, b, 0b11010010);
+            var tmp2 = Sse.Multiply(tmp0, b);
+            var tmp3 = Sse.Multiply(tmp0, tmp1);
+            var tmp4 = Sse.Shuffle(tmp2, tmp2, 0b11001001);
+            return Sse.Subtract(tmp3, tmp4);
         }
 
         public Raycast? Cast(Ray ray) => ray.Cast(this);
