babdd8fbc139ba248ad17af7dd94a2a286db3992
Helco <hermann.noll@hotmail.com>
2021-11-05 17:57:23 +0100

    Fix jump animation

--- zzre/game/components/PlayerPuppet.cs
+++ zzre/game/components/PlayerPuppet.cs
@@ -6,7 +6,6 @@ namespace zzre.game.components
     public struct PlayerPuppet
     {
         public float FallTimer;
-        public float FallAnimationTimer;
         public bool DidResetPlanarVelocity; // after switching to idling
     }
 }
