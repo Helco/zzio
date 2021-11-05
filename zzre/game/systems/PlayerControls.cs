c8362d6aa6f9cec2862e3d8a368893b86864e070
Helco <hermann.noll@hotmail.com>
2021-11-05 17:35:02 +0100

    Fix jump behavior

--- zzre/game/systems/PlayerControls.cs
+++ zzre/game/systems/PlayerControls.cs
@@ -7,12 +7,16 @@ namespace zzre.game.systems
 {
     public class PlayerControls : AComponentSystem<float, components.PlayerControls>
     {
+        private const float JumpLockDuration = 0.8f;
         private const Key ForwardKey = Key.W;
         private const Key BackwardKey = Key.S;
         private const Key LeftKey = Key.A;
         private const Key RightKey = Key.D;
         private const Key JumpKey = Key.Space;
         private readonly IZanzarahContainer zzContainer;
+
+        private float jumpLockTimer;
+        private bool jumpChanged;
         private components.PlayerControls nextControls;
 
         public PlayerControls(ITagContainer diContainer) : base(diContainer.GetTag<DefaultEcs.World>())
@@ -30,13 +34,21 @@ namespace zzre.game.systems
             zzContainer.OnKeyUp -= HandleKeyUp;
         }
 
-        protected override void Update(float state, ref components.PlayerControls component)
+        protected override void Update(float elapsedTime, ref components.PlayerControls component)
         {
+            jumpLockTimer = Math.Max(0f, jumpLockTimer - elapsedTime);
             component.GoesForward = nextControls.GoesForward;
             component.GoesBackward = nextControls.GoesBackward;
             component.GoesRight = nextControls.GoesRight;
             component.GoesLeft = nextControls.GoesLeft;
-            component.Jumps = nextControls.Jumps;
+
+            // Jump is weird, it is set by the physics system (both true and false) 
+            // so we should override it with care
+            if (jumpChanged)
+            {
+                component.Jumps = nextControls.Jumps;
+                jumpChanged = false;
+            }
         }
 
         private void HandleKeyDown(Key obj) => HandleKey(obj, true);
@@ -49,7 +61,18 @@ namespace zzre.game.systems
                 case BackwardKey: nextControls.GoesBackward = isDown; break;
                 case LeftKey: nextControls.GoesLeft = isDown; break;
                 case RightKey: nextControls.GoesRight = isDown; break;
-                case JumpKey: nextControls.Jumps = isDown; break;
+
+                case JumpKey when isDown == nextControls.Jumps:
+                    return;
+                case JumpKey when !isDown:
+                    nextControls.Jumps = false;
+                    jumpChanged = true;
+                    break;
+                case JumpKey when isDown && jumpLockTimer <= 0f:
+                    jumpLockTimer = JumpLockDuration;
+                    nextControls.Jumps = true;
+                    jumpChanged = true;
+                    break;
             }
         }
     }
