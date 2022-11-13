586418dfa44c8971f3f7de4e71dc1cb1d4216459
Helco <hermann.noll@hotmail.com>
2022-11-13 09:28:27 +0100

    Try to fix bash syntax

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -3,9 +3,9 @@ set +e
 
 git submodule update --init --recursive --single-branch .
 
-dotnet pack extern/DefaultEcs/source/DefaultEcs/DefaultEcs.csproj -c SafeDebug --include-symbols --version-suffix safe //p:TEST=true -o nuget-feed
-dotnet pack extern/DefaultEcs/source/DefaultEcs/DefaultEcs.csproj -c Release --include-symbols //p:TEST=true -o nuget-feed
-dotnet pack extern/Veldrid/src/Veldrid/Veldrid.csproj -c Release --include-symbols //p:ExcludeOpenGL=true -o nuget-feed
+dotnet pack extern/DefaultEcs/source/DefaultEcs/DefaultEcs.csproj -c SafeDebug --include-symbols --version-suffix safe "/p:TEST=true" -o nuget-feed
+dotnet pack extern/DefaultEcs/source/DefaultEcs/DefaultEcs.csproj -c Release --include-symbols "/p:TEST=true" -o nuget-feed
+dotnet pack extern/Veldrid/src/Veldrid/Veldrid.csproj -c Release --include-symbols "/p:ExcludeOpenGL=true" -o nuget-feed
 dotnet pack extern/Veldrid/src/Veldrid.MetalBindings/Veldrid.MetalBindings.csproj -c Release --include-symbols -o nuget-feed
 dotnet pack extern/Veldrid/src/Veldrid.ImageSharp/Veldrid.ImageSharp.csproj -c Release --include-symbols -o nuget-feed
 dotnet pack extern/Veldrid/src/Veldrid.SDL2/Veldrid.SDL2.csproj -c Release --include-symbols -o nuget-feed
