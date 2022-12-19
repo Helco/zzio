22d37c352b0db048e16d382f271a26930457cabd
Helco <hermann.noll@hotmail.com>
2022-12-19 12:29:07 +0100

    CI: Use reusable workflow for pr/main buidls

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,19 +15,19 @@ ImGuiNETHash=`git -C extern/ImGui.NET rev-parse --short HEAD`
 Configuration=Release
 ConfigSuffix=
 VeldridHash=notactuallyused
-SymbolFlags="--include-symbols -p:EmbedAllSources=true -p:DebugType=embedded -p:SymbolPackageFormat=snupkg -clp:ErrorsOnly"
+CommonFlags="--include-symbols -p:EmbedAllSources=true -p:DebugType=embedded -p:SymbolPackageFormat=snupkg -clp:ErrorsOnly -o nuget-feed"
 
-dotnet pack extern/DefaultEcs/source/DefaultEcs/DefaultEcs.csproj -c SafeDebug $SymbolFlags "-p:TEST=true" -o nuget-feed --version-suffix safe-$DefaultEcsHash
-dotnet pack extern/DefaultEcs/source/DefaultEcs/DefaultEcs.csproj -c $Configuration $SymbolFlags "-p:TEST=true" -o nuget-feed --version-suffix $DefaultEcsHash$ConfigSuffix
-dotnet pack extern/Veldrid/src/Veldrid/Veldrid.csproj -c $Configuration $SymbolFlags "-p:ExcludeOpenGL=true" -o nuget-feed --version-suffix $VeldridHash$ConfigSuffix
-dotnet pack extern/Veldrid/src/Veldrid.MetalBindings/Veldrid.MetalBindings.csproj -c $Configuration $SymbolFlags -o nuget-feed --version-suffix $VeldridHash$ConfigSuffix
-dotnet pack extern/Veldrid/src/Veldrid.ImageSharp/Veldrid.ImageSharp.csproj -c $Configuration $SymbolFlags -o nuget-feed --version-suffix $VeldridHash$ConfigSuffix
-dotnet pack extern/Veldrid/src/Veldrid.SDL2/Veldrid.SDL2.csproj -c $Configuration $SymbolFlags -o nuget-feed --version-suffix $VeldridHash$ConfigSuffix
-dotnet pack extern/Veldrid/src/Veldrid.StartupUtilities/Veldrid.StartupUtilities.csproj -c $Configuration $SymbolFlags -o nuget-feed --version-suffix $VeldridHash$ConfigSuffix
-dotnet pack extern/Veldrid/src/Veldrid.ImGui/Veldrid.ImGui.csproj -c $Configuration $SymbolFlags -o nuget-feed --version-suffix $VeldridHash$ConfigSuffix
-dotnet pack extern/Veldrid/src/Veldrid.RenderDoc/Veldrid.RenderDoc.csproj -c $Configuration $SymbolFlags -o nuget-feed --version-suffix $VeldridHash$ConfigSuffix
-dotnet pack extern/ImGui.NET/src/ImGui.NET/ImGui.NET.csproj -c $Configuration $SymbolFlags -o nuget-feed -p:PackagePrereleaseIdentifier=-$ImGuiNETHash$ConfigSuffix
-dotnet pack extern/ImGui.NET/src/ImGuizmo.NET/ImGuizmo.NET.csproj -c $Configuration $SymbolFlags -o nuget-feed -p:PackagePrereleaseIdentifier=-$ImGuiNETHash$ConfigSuffix
+dotnet pack extern/DefaultEcs/source/DefaultEcs/DefaultEcs.csproj -c SafeDebug $CommonFlags "-p:TEST=true" --version-suffix safe-$DefaultEcsHash
+dotnet pack extern/DefaultEcs/source/DefaultEcs/DefaultEcs.csproj -c $Configuration $CommonFlags "-p:TEST=true" --version-suffix $DefaultEcsHash$ConfigSuffix
+dotnet pack extern/Veldrid/src/Veldrid/Veldrid.csproj -c $Configuration $CommonFlags "-p:ExcludeOpenGL=true" --version-suffix $VeldridHash$ConfigSuffix
+dotnet pack extern/Veldrid/src/Veldrid.MetalBindings/Veldrid.MetalBindings.csproj -c $Configuration $CommonFlags --version-suffix $VeldridHash$ConfigSuffix
+dotnet pack extern/Veldrid/src/Veldrid.ImageSharp/Veldrid.ImageSharp.csproj -c $Configuration $CommonFlags --version-suffix $VeldridHash$ConfigSuffix
+dotnet pack extern/Veldrid/src/Veldrid.SDL2/Veldrid.SDL2.csproj -c $Configuration $CommonFlags --version-suffix $VeldridHash$ConfigSuffix
+dotnet pack extern/Veldrid/src/Veldrid.StartupUtilities/Veldrid.StartupUtilities.csproj -c $Configuration $CommonFlags --version-suffix $VeldridHash$ConfigSuffix
+dotnet pack extern/Veldrid/src/Veldrid.ImGui/Veldrid.ImGui.csproj -c $Configuration $CommonFlags --version-suffix $VeldridHash$ConfigSuffix
+dotnet pack extern/Veldrid/src/Veldrid.RenderDoc/Veldrid.RenderDoc.csproj -c $Configuration $CommonFlags --version-suffix $VeldridHash$ConfigSuffix
+dotnet pack extern/ImGui.NET/src/ImGui.NET/ImGui.NET.csproj -c $Configuration $CommonFlags -p:PackagePrereleaseIdentifier=-$ImGuiNETHash$ConfigSuffix
+dotnet pack extern/ImGui.NET/src/ImGuizmo.NET/ImGuizmo.NET.csproj -c $Configuration $CommonFlags -p:PackagePrereleaseIdentifier=-$ImGuiNETHash$ConfigSuffix
 
 # Prevent dirty submodules
 pushd extern/ImGui.NET
