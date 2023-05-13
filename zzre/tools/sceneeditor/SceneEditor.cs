47c863d92b32450c9436739aa35bbf924e3ed32b
Wullean <Wullean@users.noreply.github.com>
2023-05-13 23:59:12 +0200

    Add shortcuts for saving, deleting and duplicating

--- zzre/tools/sceneeditor/SceneEditor.cs
+++ zzre/tools/sceneeditor/SceneEditor.cs
@@ -35,6 +35,8 @@ public partial class SceneEditor : ListDisposable, IDocumentEditor
     public IResource? CurrentResource { get; private set; }
     public Window Window { get; }
 
+    private bool ControlIsPressed = false;
+
     public SceneEditor(ITagContainer diContainer)
     {
         resourcePool = diContainer.GetTag<IResourcePool>();
@@ -94,7 +96,10 @@ public partial class SceneEditor : ListDisposable, IDocumentEditor
         new WaypointComponent(localDiContainer);
         new SelectionComponent(localDiContainer);
         foModelComponent = new FOModelComponent(localDiContainer);
-        triggerComponent = new TriggerComponent(localDiContainer, menuBar);
+        triggerComponent = new TriggerComponent(localDiContainer);
+        Window.OnKeyUp += HandleKeyUp;
+        Window.OnKeyDown += HandleKeyDown;
+        Window.OnContent += HandleOnContent;
         diContainer.GetTag<OpenDocumentSet>().AddEditor(this);
     }
 
@@ -105,6 +110,32 @@ public partial class SceneEditor : ListDisposable, IDocumentEditor
         assetRegistry.Dispose();
     }
 
+    private void HandleKeyDown(Key key)
+    {
+        if (key == Key.ControlLeft)
+            ControlIsPressed = true;
+    }
+    private void HandleKeyUp(Key key)
+    {
+        if (key == Key.ControlLeft)
+            ControlIsPressed = false;
+        else if (ControlIsPressed)
+        {
+            if (key == Key.D)
+                DuplicateCurrentSelection();
+            else if (key == Key.S)
+                SaveScene();
+            else if (key == Key.X)
+                DeleteCurrentSelection();
+        }
+    }
+    private void HandleOnContent()
+    {
+        if (Window.IsFocused == false)
+        {
+            ControlIsPressed = false;
+        }
+    }
     private void DuplicateCurrentSelection()
     {
         triggerComponent.DuplicateCurrentTrigger();
@@ -164,4 +195,5 @@ public partial class SceneEditor : ListDisposable, IDocumentEditor
         var stream = new FileStream(CurrentResource.Path.Absolute.ToString(), FileMode.Create);
         scene.Write(stream);
     }
+
 }
