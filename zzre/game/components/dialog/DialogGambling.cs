73b4773535bfcb3fcb53d0ce126eeae95e3e9404
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2024-02-07 18:05:34 +0300

    Shuffling a couple lines

--- zzre/game/components/dialog/DialogGambling.cs
+++ zzre/game/components/dialog/DialogGambling.cs
@@ -8,13 +8,16 @@ namespace zzre.game.components;
 public struct DialogGambling
 {
     public DefaultEcs.Entity DialogEntity;
+
     public ItemRow Currency;
-    public SpellRow? Purchase;
     public List<int?> Cards;
     public List<SpellRow?> SelectedCards;
+    public SpellRow? Purchase;
+
     public Rect bgRect;
     public Dictionary<components.ui.ElementId, SpellRow> CardPurchaseButtons;
     public DefaultEcs.Entity CurrencyLabel;
     public DefaultEcs.Entity Profile;
+
     public float? RowAnimationTimeLeft;
 }
